0f765127054699dfdfa1691b5431c00d1f13539b
Wahyu Febri <137754909+wahyuzero@users.noreply.github.com>
2024-02-26 19:26:05 +0700

    Changed for multi api

--- Spam_api.py
+++ Spam_api.py
@@ -1,39 +1,34 @@
 import requests
 import time
 
-def send_telegram_message(bot_token, chat_id, stringExtra, stringExtra2, stringExtra3):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    headers = {'Content-Type': 'application/json'}
-    message = f"*{stringExtra}*%0A%0A*From:* _{stringExtra2}_%0A*Message:* _{stringExtra3}_"
-    payload = {
-        'parse_mode': 'markdown',
-        'chat_id': chat_id,
-        'text': message
-    }
+def send_message(api_url, message, api_number):
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            print(f"API {api_number} = Success {response.status_code} OK")
+            return True, response.json()
+        else:
+            print(f"API {api_number} = {response.status_code} {response.reason}")
+            return False, response.json()
+    except Exception as e:
+        print(f"Error sending message to API {api_number}: {str(e)}")
+        return False, None
 
-    response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200:
-        print("Message sent successfully!")
-    else:
-        print(f"Failed to send message. Status code: {response.status_code}")
-        print(response.text)
-    return response
+def main():
+    api_urls = [
+        "https://api.telegram.org/bot7073742992:AAF3fBCHGAcvpPeHdt-xyfLC15xn8X-k02o/sendMessage?parse_mode=markdown&chat_id=6050538291&text=Bang-ada-Undangan-dari-mantan-nih",
+        "https://api.telegram.org/bot6928730988:AAHKy88YEVBSX3Ne9AEExbJ7BwViDleRMqo/sendMessage?parse_mode=markdown&chat_id=6973831185&text=Bang-ada-Undangan-dari-mantan-nih"
+    ]
 
-def log_response(response, filename="response_log.txt"):
-    with open(filename, "a") as file:
-        file.write("Timestamp: " + time.strftime('%Y-%m-%d %H:%M:%S') + "\n")
-        file.write(str(response.json()) + "\n\n")
-# Api Token
-bot_token = "6928730988:AAHKy88YEVBSX3Ne9AEExbJ7BwViDleRMqo"
-chat_id = "6973831185"
-# Message
-stringExtra = "Pakyu"
-stringExtra2 = "Bitchhh"
-stringExtra3 = "Loremipsummmmmmm"
+    while True:
+        for i, api_url in enumerate(api_urls, start=1):
+            success, response = send_message(api_url, "  ", i)
+            if response:
+                # Log the response
+                with open("message_log.txt", "a") as f:
+                    f.write(f"API {i} : {response}\n")
 
-while True:
-    response = send_telegram_message(bot_token, chat_id, stringExtra, stringExtra2, stringExtra3)
-    if response:
-        log_response(response)
-        print("Response from Telegram API logged.")
-    time.sleep(0.01)
+        time.sleep(0.001)
+
+if __name__ == "__main__":
+    main()
